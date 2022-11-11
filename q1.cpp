32937fc9ac5d5d89253c4d7a5e16d86e0a6dd24a
rhrastic <ryanhrastich@yahoo.com>
2022-11-11 00:01:42 -0600

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+  int entry, sr, square;
+  do{
+    cout << "\nEnter number to see if it is a perfect square: ";
+    cin >> entry;
+    sr = sqrt(entry);
+    square = sr*sr;
+  }
+  while(entry != square);
+  cout << "\nCorrect!";
+  return 0;
+}
